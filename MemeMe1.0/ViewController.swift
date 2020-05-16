d153cda8504b39d4836e9c6b156f3ad34572756b
Vanessa Chau <vanessaschau@gmail.com>
2020-05-16 15:59:11 -0700

    Initial Commit

---
+++ MemeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by Vanessa on 5/16/20.
+//  Copyright © 2020 Vanessa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
